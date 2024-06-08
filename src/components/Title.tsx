5af2b805a05999412e3d29679c3177ff127912a5
日高敬介 <keisuke877jp@yahoo.co.jp>
2024-06-08 23:12:36 +0900

    :lipstick: タイトルテキストを相対位置に修正

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,9 +4,9 @@ import MeltingIcon from "../assets/melting.svg";
 const Title :FC = () => {
     return (
         <>
-            <div className="flex flex-col items-center py-24 ">
-                <img className="absolute top-24 opacity-10 avatar" src={MeltingIcon} alt={'melting'} style={{width: 250, height: 250}} />
-                <div className="absolute top-40 flex flex-col text-center">
+            <div className="flex flex-col pb-52 m-8 items-center">
+                <img className="absolute opacity-10 avatar" src={MeltingIcon} alt={'melting'} style={{width: 250, height: 250}} />
+                <div className="absolute pt-10 flex flex-col text-center">
                     <span className="text-7xl font-Andika text-stroke">totalizer</span>
                     <span className="text-3xl font-Andika">YES/NOアンケート</span>
                 </div>                
