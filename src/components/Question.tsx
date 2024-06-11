c39f3b30fbeca80793db11c902761273e35b160b
日高敬介 <keisuke877jp@yahoo.co.jp>
2024-06-11 23:34:31 +0900

    :sparkles: Vote画面の質問を編集可能に変更

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -3,9 +3,16 @@ import { FC } from "react";
 export const Question: FC = () => {
     return (
         <>
-            <div className="flex flex-col mt-24 bg-white items-center justify-center rounded-2xl h-2/3 max-w-72 w-screen">
-            ここに質問を書きます?
-            </div>
+            {/* <div className="flex flex-col m-5 bg-white items-center justify-center rounded-2xl h-screen w-11/12">
+                ここに質問を書きます?
+            </div> */}
+            <input
+                className="flex flex-col m-5 bg-white text-center justify-center rounded-2xl h-screen w-11/12"
+                type="text"
+                defaultValue='ここに質問を書きます?'
+                // value={'ここに質問を書きます?'}
+                onChange={(e) => e.preventDefault()}
+            />
         </>
     )
 }
--- src/screens/Vote.tsx
+++ src/screens/Vote.tsx
@@ -8,21 +8,16 @@ export const Vote: FC = () => {
   return (
     <>
       <div className="flex flex-col items-center justify-center bg-blue-50 h-dvh w-screen">
-        <div className="flex flex-col items-center bg-indigo-200 h-1/2 max-w-96 w-screen">
-          <Question />
+        <div className="flex flex-col items-center bg-blue-50 h-1/2 max-w-96 w-screen">
+          <div className="flex flex-col items-center bg-indigo-200 h-5/6 max-w-96 w-screen">
+            <Question />
+          </div>
         </div>
         <div className="flex flex-col items-center bg-blue-50 h-1/2 w-screen">
           <Button text='YES' bgColor='bg-emerald-600' txtColor='text-white' center onClick={() => navigate('/')}/>
           <Button text='NO' bgColor='bg-rose-600' txtColor='text-white' center />
         </div>
       </div>
-      {/* <div className="flex flex-col items-center bg-blue-50 justify-center h-dvh w-screen">
-        
-        <Question />
-        <div className=" bg-blue-600 inset-y-0 h-1/2 w-screen"></div>
-        <Button text='YES' bgColor='bg-emerald-600' txtColor='text-white' center />
-        <Button text='NO' bgColor='bg-rose-600' txtColor='text-white' center />
-      </div> */}
     </>
   )
 }
