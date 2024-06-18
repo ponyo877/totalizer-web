32a9a68991d0a32796fbeffce3a038431b0b2c4b
日高敬介 <keisuke877jp@yahoo.co.jp>
2024-06-19 00:46:12 +0900

    :adhesive_bandage: 結果表示ボタンの修正

--- src/components/FAB.tsx
+++ src/components/FAB.tsx
@@ -10,12 +10,12 @@ type fabOption = {
 };
 
 export const FAB: FC<fabOption> = (op: fabOption) => {
-    const txtColor = op.txtColor ? op.txtColor : 'text-black';
+    const txtColor = op.txtColor ? op.txtColor : 'text-white';
     const bgColor = op.bgColor ? op.bgColor : 'bg-white';
     // bottom-10 right-10 
     return (
         <div
-            className={`${op.coordinate} py-2 px-2 border-2 ${bgColor} ${txtColor} rounded-full cursor-pointer`}
+            className={`${op.coordinate} p-2 text-2xl ${bgColor} ${txtColor} rounded-2xl w-[50%] shadow-lg font-Andika cursor-pointer`}
             onClick={() => window.open("https://www.google.com/?hl=ja")}>
             {op.text}
         </div>
--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -4,25 +4,15 @@ import { FAB } from "./FAB";
 export const Question: FC = () => {
     return (
         <>
-            {/* <div className="flex flex-col m-5 bg-white items-center justify-center rounded-2xl h-screen w-11/12">
-                ここに質問を書きます?
-            </div> */}
-            {/* <input
-                className="flex flex-col m-5 bg-white text-center justify-center rounded-2xl h-screen w-11/12"
-                type="text"
-                defaultValue='ここに質問を書きます?'
-                onChange={(e) => e.preventDefault()}
-            /> */}
-            {/* <FAB coordinate="my-48" text="ぽにょ"/> */}
-            <div className="relative flex flex-col items-center justify-center h-screen w-11/12">
+            <div className="relative flex flex-col items-center justify-center h-screen max-w-96 w-screen">
                 <input
-                    className="absolute flex flex-col bg-white text-center rounded-2xl h-5/6 w-11/12"
+                    className="absolute flex flex-col bg-white text-center rounded-2xl h-[90%] w-[90%]"
                     type="text"
                     defaultValue='ここに質問を書きます?'
                     onChange={(e) => e.preventDefault()}
                 />
-                <div className="absolute top-3/4 flex flex-col text-center">
-                    <FAB text="ぽにょ" />
+                <div className="absolute top-[85%] flex flex-col items-center text-center max-w-96 w-screen">
+                    <FAB text="結果発表" bgColor="bg-indigo-400" />
                 </div>
             </div>
         </>
