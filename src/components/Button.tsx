0956097ba9b198798b7f24074b23c2ff409f14dc
ponyo877 <keisuke877jp@yahoo.co.jp>
2025-01-27 22:30:21 +0900

    packageのversion up

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -14,7 +14,7 @@ const Button: FC<buttonOption> = (op: buttonOption) => {
     const bgColor = op.bgColor ? op.bgColor : 'bg-white';
     return (
         <label
-            className={`flex items-center ${op.center && 'justify-center'} h-20 w-64 min-w-2/3 px-6 m-4 text-3xl duration-150 ${bgColor} ${txtColor} rounded-2xl shadow-lg font-Andika cursor-pointer`}
+            className={`flex items-center ${op.center && 'justify-center'} h-20 w-64 min-w-1/3 px-6 m-4 text-3xl duration-150 ${bgColor} ${txtColor} rounded-2xl shadow-lg font-Andika cursor-pointer`}
             onClick={op.onClick}>
             {op.icon && <div className="pr-6">{op.icon}</div>}{op.text}
         </label>);
